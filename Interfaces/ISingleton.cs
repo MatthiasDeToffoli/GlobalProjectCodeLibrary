c4af79fa1aee4e7e69e17d872f5382bb15e07bff
MatthiasDeToffoli <matthias.detoffoli@gmail.com>
2020-05-19 18:09:54 +0200

    [UPDATE] fix the namespace of the singleton interface

--- Interfaces/ISingleton.cs
+++ Interfaces/ISingleton.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace fr.matthiasdetoffoli.GlobalProjectCode.Interfaces
+﻿namespace fr.matthiasdetoffoli.GlobalProjectCode.Interfaces
 {
     /// <summary>
     /// Singleton Interface
